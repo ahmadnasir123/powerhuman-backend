36e74ae1106da912b561f1fdc4843a15b22442ef
ahmadnasir123 <nasirahmad240502@gmail.com>
2023-09-12 21:45:29 +0700

    Update 12 Sept 2023

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'icon',
+        'company_id'
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function empployees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
