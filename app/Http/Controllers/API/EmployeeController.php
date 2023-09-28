9082a222671bf8137c2d2eb791862e71adf30999
ahmadnasir123 <nasirahmad240502@gmail.com>
2023-09-28 11:45:58 +0700

    Add company to employee API

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -23,6 +23,7 @@ class EmployeeController extends Controller
         $phone = $request->input('phone');
         $team_id = $request->input('team_id');
         $role_id = $request->input('role_id');
+        $company_id = $request->input('company_id');
         $limit = $request->input('limit', 10);
 
         //powerhuman.com/api/employee?id=1
@@ -64,6 +65,11 @@ class EmployeeController extends Controller
         }
         if ($role_id) {
             $employees->where('role_id', $role_id);
+        }
+        if ($company_id) {
+            $employees->whereHas('team', function ($query) use ($company_id) {
+                $query->where('company_id', $company_id);
+            });
         }
 
         return ResponseFormatter::success(
